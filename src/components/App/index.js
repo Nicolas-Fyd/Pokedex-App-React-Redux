29d32766061e6795c6516682bd7ed0f1e34f8e14
Nicolas <nicolasfayard2812@gmail.com>
2023-10-20 16:22:41 +0200

    mise en place du react-modele

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import logo from 'src/assets/logo.png';
+import './styles.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <img src={logo} alt="react logo" />
+    </div>
+  );
+}
+
+export default App;
