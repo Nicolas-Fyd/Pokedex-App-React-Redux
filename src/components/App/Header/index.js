a4c4aa9b662578dba1d20d7b180ec449826c04a0
Nicolas <nicolasfayard2812@gmail.com>
2023-11-19 15:13:34 +0100

    mise en place des éléments du state + l'action + reducer pour la méthode changeField qui gère les champs contrôlés email et password

--- src/components/App/Header/index.js
+++ src/components/App/Header/index.js
@@ -1,14 +1,33 @@
 import logo from 'src/assets/logo2.png';
+import { useSelector, useDispatch } from 'react-redux';
 import LoginForm from './LoginForm';
 import './styles.scss';
+import { changeLoginField } from '../../../actions/user';
 
 function Header() {
+  const email = useSelector((state) => state.user.email);
+  const password = useSelector((state) => state.user.password);
+
+  const dispatch = useDispatch();
+
   return (
     <div className="header">
       <a href="/">
         <img className="logo" src={logo} alt="react logo" />
       </a>
-      <LoginForm />
+      <LoginForm
+        email={email}
+        password={password}
+        changeField={(newValue, identifier) => {
+          dispatch(changeLoginField(newValue, identifier));
+        }}
+        handleLogin={() => {
+          console.log('handleLogin');
+        }}
+        handleLogout={() => {
+          console.log('handleLogout');
+        }}
+       />
     </div>
   );
 }
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,8 +1,19 @@
+import { CHANGE_LOGIN_FIELD } from '../actions/user';
+
 export const initialState = {
+  isLogged: false,
+  email: '',
+  password: '',
 };
 
 const reducer = (state = initialState, action = {}) => {
   switch (action.type) {
+    case CHANGE_LOGIN_FIELD:
+      return {
+        ...state,
+        // property_accessors
+        [action.identifier]: action.newValue,
+      };
     default:
       return state;
   }
