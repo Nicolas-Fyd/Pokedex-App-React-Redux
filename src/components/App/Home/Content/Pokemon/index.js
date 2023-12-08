bb26b6dd6ffb9edfb4f0f373b20dc2e5a6b3d9a4
Nicolas <nicolasfayard2812@gmail.com>
2023-12-08 15:14:36 +0100

    ajout de l'affichage condition du + sur les pookemons lorsque l'utilisateur est connecté

--- src/components/App/Home/Content/Pokemon/index.js
+++ src/components/App/Home/Content/Pokemon/index.js
@@ -1,9 +1,20 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import plus from 'src/assets/plus.png';
 import './styles.scss';
 
 function Pokemon({ ...pokemon }) {
+  const isLogged = useSelector((state) => state.user.isLogged);
+
   return (
     <div className="pokemon">
+      {isLogged && (
+      <div className="logo-container">
+        <Link to="/">
+          <img className="logo-img" src={plus} alt="Votre logo" />
+        </Link>
+      </div>
+      )}
       <Link to={pokemon.name}>
         <img className="pokemon-img" src={pokemon.image} alt={pokemon.name} />
       </Link>
